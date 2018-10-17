2e88b087c3efe4606aa700ef3bc13a975168c4ae
uribench <uribench@gmail.com>
2018-10-17 19:07:07 +0000

    Add cipher tests

--- deranged_alphabet_cipher/tests/encoder_decoder_test.py
+++ deranged_alphabet_cipher/tests/encoder_decoder_test.py
@@ -3,6 +3,22 @@ from deranged_alphabet_cipher.src.encoder_decoder import *
 
 class EncoderDecoder(unittest.TestCase):
 
+    def test_cipher_with_salt_equal_numeric_value(self):
+        with self.assertRaises(AttributeError):
+            make_cipher(0)
+
+    def test_cipher_with_salt_equal_alphanumeric_value(self):
+        with self.assertRaises(SystemExit):
+            make_cipher('A0')
+
+    def test_cipher_with_salt_equal_alphabet_no_duplicates_value(self):
+        result = make_cipher('QWERTY')
+        self.assertEqual('Q', result[0])
+
+    def test_cipher_with_salt_equal_alphabet_with_duplicates_value(self):
+        result = make_cipher('QQWERTY')
+        self.assertEqual('a', result[6])
+
     def test_encode_with_null_string(self):
         self.assertEqual(encode('', cipher), '', 'Null Value')
 
