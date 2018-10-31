0434c96835f93a05f9182297148f3e3742d102bf
uribench <uribench@gmail.com>
2018-10-31 06:17:37 +0000

    Avoid side-effects when mutating a common object

--- book_ciphers/tests/decoder_book_cipher_async.spec.js
+++ book_ciphers/tests/decoder_book_cipher_async.spec.js
@@ -1,12 +1,15 @@
 const decode = require('../src/decoder_book_cipher_async');
-const secret_1 = require('../src/secrets/secret_1');
-const secret_2 = require('../src/secrets/secret_2');
+const secret = require('../src/secrets/secret_1');
 
 describe('decoder_book_cipher_async', function () {
   it('should decode the secret object successfully', () => {
     let result = 'Remove';
+    // let secret_module = '../src/secrets/secret_1'
 
-    decode(secret_1).then((res) => {
+    // delete require.cache[require.resolve(secret_module)]
+    // var secret = require(secret_module);
+
+    decode(secret).then((res) => {
       expect(res).toBe(result);
     });
   });
--- book_ciphers/tests/decoder_book_cipher_sync.spec.js
+++ book_ciphers/tests/decoder_book_cipher_sync.spec.js
@@ -1,9 +1,9 @@
 const decode = require('../src/decoder_book_cipher_sync');
-const secret = require('../src/secrets/secret_2');
+const secret = require('../src/secrets/secret_1');
 
 describe('decoder_book_cipher_sync', function () {
   it('should decode the secret object successfully', () => {
-    let result = 'Pass';
+    let result = 'Remove';
 
     expect(decode(secret)).toBe(result);
   });
